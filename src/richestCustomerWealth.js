43451cae8ab754795535e2de46e06a4906c0ffff
Rabie BELKAIDA <60550640+rabiebe@users.noreply.github.com>
2023-07-23 15:16:18 +0100

    Initial commit

---
+++ src/richestCustomerWealth.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+var maximumWealth = function (accounts) {
+  let richest = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    const element = accounts[i];
+    let sum = calculSum(element);
+    richest = Math.max(richest, sum);
+  }
+  return richest;
+};
+
+var calculSum = (numbers) => {
+  return numbers.reduce((a, b) => a + b);
+};
+
+let result = maximumWealth([
+  [2, 8, 7],
+  [7, 1, 3],
+  [1, 9, 5]
+]);
+
+console.log(result);
